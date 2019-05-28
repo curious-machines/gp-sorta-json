19cc56e44e413f729184218859ae0120487f97ad
Kevin Lindsey <kevin@kevlindev.com>
2019-05-28 14:28:24 -0700

    Test some primitives

---
+++ test/parser-test.js
@@ -0,0 +1,24 @@
+import assert from "assert";
+import Parser from "../lib/Parser.js";
+
+describe("Parser", () => {
+    describe("Primitives", () => {
+        const tests = [
+            { source: "true", expected: true },
+            { source: "false", expected: false },
+            { source: "null", expected: null },
+            { source: "undefined", expected: undefined },
+            { source: "10.5", expected: 10.5 },
+            { source: '"test"', expected: "test" }
+        ];
+
+        tests.forEach(test => {
+            it(test.source, () => {
+                const {source, expected} = test;
+                const result = Parser.parse(source);
+
+                assert.deepStrictEqual(result, expected);
+            });
+        });
+    });
+});
